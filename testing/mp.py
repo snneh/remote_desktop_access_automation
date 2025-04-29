650a2aac82c0dce11ffdc43bd4efc697516c14b3
sneh <snehanshu.singh1@gmail.com>
2025-04-29 18:22:11 +0530

    tryna implement multiprocessing

---
+++ testing/mp.py
@@ -0,0 +1,36 @@
+# running tasks in parallel on different cpu cores, bypasses GIL used for threads.
+# better for CPU bound tasks (heavy CPU usage)
+
+from multiprocessing import Process, cpu_count
+import time
+
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+
+def main():
+    print(cpu_count())
+
+    a = Process(target=counter, args=(250000000,))
+    b = Process(target=counter, args=(250000000,))
+    c = Process(target=counter, args=(250000000,))
+    d = Process(target=counter, args=(250000000,))
+
+    a.start()
+    b.start()
+    c.start()
+    d.start()
+
+    a.join()
+    b.join()
+    c.join()
+    d.join()
+
+    print(f"finshed in : {time.perf_counter()} seconds")
+
+
+if __name__ == "__main__":
+    main()
